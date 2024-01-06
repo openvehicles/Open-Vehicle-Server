367a2a2abc4a40c1271d2ba7a29f2faefdec3bd9
Michael Balzer <balzer@expeedo.de>
2024-01-06 17:20:30 +0100

    PushGCM: use child process for message delivery, fix ticker registration

--- v3/server/plugins/system/OVMS/Server/PushGCM.pm
+++ v3/server/plugins/system/OVMS/Server/PushGCM.pm
@@ -40,13 +40,13 @@ our @EXPORT = qw();
 
 my $me; # Reference to our singleton object
 my @gcm_queue;
-my $gcm_running=0;
-my $gcm_interval;
+my $gcm_running;
 my $gcm_api_key_file;
 my $gcm_api_key_json;
 my $gcm_project_id;
 my $gcm_api_url;
 my $gcm_con;
+my $gcm_timer;
 
 if (!PluginLoaded('Push'))
   {
@@ -111,8 +111,8 @@ sub new
     }
 
   # Start ticker:
-  $gcm_interval = MyConfig()->val('gcm','interval',10);
-  my $gcmtim = AnyEvent->timer (after => $gcm_interval, interval => $gcm_interval, cb => \&gcm_tim);
+  my $gcm_interval = MyConfig()->val('gcm','interval',10);
+  $gcm_timer = AnyEvent->timer (after => $gcm_interval, interval => $gcm_interval, cb => \&gcm_tim);
 
   return $self;
   }
@@ -129,82 +129,98 @@ sub PushNotifyGCM
 
   push @gcm_queue,$rec;
 
-  &gcm_tim();
-
   return;
   }
 
 sub PushNow
   {
-  &gcm_tim() if ($gcm_running==0);
+  &gcm_tim();
   }
 
 sub gcm_tim
   {
-  return if ($gcm_running>0);
+  return if (!defined $gcm_con);
+  return if (defined $gcm_running);
   return if (scalar @gcm_queue == 0);
 
-  return if (!defined $gcm_con);
-
-  AE::log info => "- - - msg gcm processing ".(scalar @gcm_queue)." queued notification(s)";
-
-  # Potential issue: WWW::FCM::HTTP::V1->send() is synchronous, needs ~0.3 seconds per call.
-  # Do we need to restrict the number of messages processed per ticker run?
-  # Is there an async option?
-  $gcm_running = 1;
-  foreach my $rec (@gcm_queue)
+  # WWW::FCM::HTTP::V1->send() is synchronous, needs ~0.3 seconds per call.
+  # Fork child process for asynchronous push message delivery:
+  my $pid = fork;
+  if (!defined $pid)
     {
-    my $owner = $rec->{'owner'};
-    my $vehicleid = $rec->{'vehicleid'};
-    my $alerttype = $rec->{'alerttype'};
-    my $alertmsg = $rec->{'alertmsg'};
-    my $timestamp = $rec->{'timestamp'};
-    my $pushkeyvalue = $rec->{'pushkeyvalue'};
-    my $appid = $rec->{'appid'};
-    AE::log info => "- - $vehicleid msg gcm '$alertmsg' => $pushkeyvalue";
+    AE::log error => "- - - msg gcm processing: fatal: cannot create child process";
+    return;
+    }
 
-    my $res = $gcm_con->send(
-      {
-      message =>
-        {
-        token          => $pushkeyvalue,
-        data =>
-          {
-          type         => $alerttype,
-          time         => $timestamp,
-          title        => $vehicleid,
-          message      => $alertmsg,
-          },
-        android =>
-          {
-          collapse_key => unpack("H*", sha256($alerttype . $timestamp . $vehicleid . $alertmsg)),
-          },
-        },
-      });
+  if ($pid == 0)
+    {
+    # we're the child process
+    AE::log info => "- - - msg gcm processing ".(scalar @gcm_queue)." queued notification(s)";
 
-    if ($res->is_success)
+    foreach my $rec (@gcm_queue)
       {
-      AE::log debug => "- - $vehicleid msg gcm message sent to $pushkeyvalue";
-      }
-    else
-      {
-      AE::log trace => "- - $vehicleid msg gcm failure response: " . $res->{'content'};
-      my $rescont = decode_json($res->{'content'});
-      my $errcode = $rescont->{'error'}{'code'};
-      my $errmsg = $rescont->{'error'}{'message'};
-      AE::log error => "- - $vehicleid msg gcm error $errcode on $pushkeyvalue: $errmsg";
-      # App instance unregistered from FCM?
-      # see https://firebase.google.com/docs/reference/fcm/rest/v1/ErrorCode
-      if ($errcode == 404)
+      my $owner = $rec->{'owner'};
+      my $vehicleid = $rec->{'vehicleid'};
+      my $alerttype = $rec->{'alerttype'};
+      my $alertmsg = $rec->{'alertmsg'};
+      my $timestamp = $rec->{'timestamp'};
+      my $pushkeyvalue = $rec->{'pushkeyvalue'};
+      my $appid = $rec->{'appid'};
+      AE::log info => "- - $vehicleid msg gcm '$alertmsg' => $pushkeyvalue";
+
+      my $res = $gcm_con->send(
         {
-        AE::log info => "- - $vehicleid msg gcm unregister $appid";
-        FunctionCall('DbUnregisterPushNotify',$owner,$vehicleid,$appid);
+        message =>
+          {
+          token          => $pushkeyvalue,
+          data =>
+            {
+            type         => $alerttype,
+            time         => $timestamp,
+            title        => $vehicleid,
+            message      => $alertmsg,
+            },
+          android =>
+            {
+            collapse_key => unpack("H*", sha256($alerttype . $timestamp . $vehicleid . $alertmsg)),
+            },
+          },
+        });
+
+      if ($res->is_success)
+        {
+        AE::log debug => "- - $vehicleid msg gcm message sent to $pushkeyvalue";
+        }
+      else
+        {
+        AE::log trace => "- - $vehicleid msg gcm failure response: " . $res->{'content'};
+        my $rescont = decode_json($res->{'content'});
+        my $errcode = $rescont->{'error'}{'code'};
+        my $errmsg = $rescont->{'error'}{'message'};
+        AE::log error => "- - $vehicleid msg gcm error $errcode on $pushkeyvalue: $errmsg";
+        # App instance unregistered from FCM?
+        # see https://firebase.google.com/docs/reference/fcm/rest/v1/ErrorCode
+        if ($errcode == 404)
+          {
+          AE::log info => "- - $vehicleid msg gcm unregister $appid";
+          FunctionCall('DbUnregisterPushNotify',$owner,$vehicleid,$appid);
+          }
         }
       }
+    # exit child process
+    exit 0;
     }
-  $gcm_running = 0;
-  @gcm_queue = ();
-  AE::log info => "- - - msg gcm processing finished";
+  else
+    {
+    # we're the main process
+    $gcm_running = AnyEvent->child (pid => $pid, cb => sub
+      {
+      AE::log info => "- - - msg gcm processing finished";
+      undef $gcm_running;
+      }
+    );
+    @gcm_queue = ();
+    }
   }
 
 1;
