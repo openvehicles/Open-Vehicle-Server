ccea487e747e059b9286bca430747c650303ce02
Michael Balzer <balzer@expeedo.de>
2024-04-09 19:49:15 +0200

    PushGCM: improve stability:  - use AnyEvent::Util fork_call for child process spawning  - do all result handling in main process  - catch FCM timeouts & JSON errors  - unregister invalid FCM tokens  - reinit FCM connection by timer (default: every hour)

--- v3/server/plugins/system/OVMS/Server/PushGCM.pm
+++ v3/server/plugins/system/OVMS/Server/PushGCM.pm
@@ -24,6 +24,7 @@ use Carp;
 use AnyEvent;
 use AnyEvent::Log;
 use AnyEvent::HTTP;
+use AnyEvent::Util;
 use URI::Escape;
 use OVMS::Server::Core;
 use OVMS::Server::Plugin;
@@ -40,13 +41,14 @@ our @EXPORT = qw();
 
 my $me; # Reference to our singleton object
 my @gcm_queue;
-my $gcm_running;
+my $gcm_running = 0;
 my $gcm_api_key_file;
 my $gcm_api_key_json;
 my $gcm_project_id;
 my $gcm_api_url;
 my $gcm_con;
 my $gcm_timer;
+my $gcm_timer_reinit;
 
 if (!PluginLoaded('Push'))
   {
@@ -102,17 +104,17 @@ sub new
   if (defined $gcm_project_id)
     {
     $gcm_api_url = "https://fcm.googleapis.com/v1/projects/$gcm_project_id/messages:send";
-    $gcm_con = WWW::FCM::HTTP::V1->new(
-      {
-      api_url      => $gcm_api_url,
-      api_key_json => $gcm_api_key_json,
-      });
+    &gcm_init_con();
     AE::log info => "- - - GCM initialized for project $gcm_project_id, API URL $gcm_api_url";
     }
 
   # Start ticker:
   my $gcm_interval = MyConfig()->val('gcm','interval',10);
   $gcm_timer = AnyEvent->timer (after => $gcm_interval, interval => $gcm_interval, cb => \&gcm_tim);
+
+  # Start connection reinit ticker:
+  my $gcm_interval_reinit = MyConfig()->val('gcm','interval_reinit',3600);
+  $gcm_timer_reinit = AnyEvent->timer (after => $gcm_interval_reinit, interval => $gcm_interval_reinit, cb => \&gcm_init_con);
 
   return $self;
   }
@@ -137,37 +139,47 @@ sub PushNow
   &gcm_tim();
   }
 
+sub gcm_init_con
+  {
+  if (defined $gcm_con)
+    {
+    undef $gcm_con;
+    }
+  $gcm_con = WWW::FCM::HTTP::V1->new(
+    {
+    api_url      => $gcm_api_url,
+    api_key_json => $gcm_api_key_json,
+    });
+  AE::log info => "- - - msg gcm connection initialized: " . $gcm_con;
+  }
+
 sub gcm_tim
   {
   return if (!defined $gcm_con);
-  return if (defined $gcm_running);
+  return if ($gcm_running == 1);
   return if (scalar @gcm_queue == 0);
 
   # WWW::FCM::HTTP::V1->send() is synchronous, needs ~0.3 seconds per call.
   # Fork child process for asynchronous push message delivery:
-  my $pid = fork;
-  if (!defined $pid)
-    {
-    AE::log error => "- - - msg gcm processing: fatal: cannot create child process";
-    return;
-    }
 
-  if ($pid == 0)
-    {
-    # we're the child process
-    AE::log info => "- - - msg gcm processing ".(scalar @gcm_queue)." queued notification(s)";
+  AE::log info => "- - - msg gcm processing queue: " . scalar @gcm_queue . " messages";
 
-    foreach my $rec (@gcm_queue)
+  if (my $rec = pop(@gcm_queue))
+    {
+    my $owner = $rec->{'owner'};
+    my $vehicleid = $rec->{'vehicleid'};
+    my $alerttype = $rec->{'alerttype'};
+    my $alertmsg = $rec->{'alertmsg'};
+    my $timestamp = $rec->{'timestamp'};
+    my $pushkeyvalue = $rec->{'pushkeyvalue'};
+    my $appid = $rec->{'appid'};
+    AE::log info => "- - $vehicleid msg gcm '$alertmsg' => $pushkeyvalue";
+
+    $gcm_running = 1;
+
+    fork_call
       {
-      my $owner = $rec->{'owner'};
-      my $vehicleid = $rec->{'vehicleid'};
-      my $alerttype = $rec->{'alerttype'};
-      my $alertmsg = $rec->{'alertmsg'};
-      my $timestamp = $rec->{'timestamp'};
-      my $pushkeyvalue = $rec->{'pushkeyvalue'};
-      my $appid = $rec->{'appid'};
-      AE::log info => "- - $vehicleid msg gcm '$alertmsg' => $pushkeyvalue";
-
+      # child process:
       my $res = $gcm_con->send(
         {
         message =>
@@ -186,41 +198,53 @@ sub gcm_tim
             },
           },
         });
-
-      if ($res->is_success)
+      return $res;
+      }
+    sub
+      {
+      # process child result:
+      my $res = shift;
+      if (!defined $res)
         {
-        AE::log debug => "- - $vehicleid msg gcm message sent to $pushkeyvalue";
+        # fork error, or FCM timeout / SSL error signaled by croak in WWW::FCM::HTTP::V1->send():
+        AE::log error => "- - $vehicleid msg gcm fork_call error $!: $@";
+        }
+      elsif ($res->is_success)
+        {
+        AE::log info => "- - $vehicleid msg gcm message sent to $pushkeyvalue";
         }
       else
         {
-        AE::log trace => "- - $vehicleid msg gcm failure response: " . $res->{'content'};
-        my $rescont = decode_json($res->{'content'});
-        my $errcode = $rescont->{'error'}{'code'};
-        my $errmsg = $rescont->{'error'}{'message'};
-        AE::log error => "- - $vehicleid msg gcm error $errcode on $pushkeyvalue: $errmsg";
-        # App instance unregistered from FCM?
-        # see https://firebase.google.com/docs/reference/fcm/rest/v1/ErrorCode
-        if ($errcode == 404)
+        # probably an FCM failure response (JSON):
+        try
           {
-          AE::log info => "- - $vehicleid msg gcm unregister $appid";
-          FunctionCall('DbUnregisterPushNotify',$owner,$vehicleid,$appid);
+          AE::log debug => "- - $vehicleid msg gcm failure response: " . $res->{'content'};
+          my $rescont = decode_json($res->{'content'});
+          my $errcode = $rescont->{'error'}{'code'};
+          my $errmsg = $rescont->{'error'}{'message'};
+          AE::log error => "- - $vehicleid msg gcm error $errcode on $pushkeyvalue: $errmsg";
+          # App instance unregistered from FCM?
+          # see https://firebase.google.com/docs/reference/fcm/rest/v1/ErrorCode
+          if ($errcode == 403 || $errcode == 404
+              || $errmsg eq "The registration token is not a valid FCM registration token")
+            {
+            AE::log info => "- - $vehicleid msg gcm unregister $appid (error $errcode $errmsg)";
+            FunctionCall('DbUnregisterPushNotify',$owner,$vehicleid,$appid);
+            }
           }
+        catch
+          {
+          # some other error:
+          AE::log error => "- - $vehicleid msg gcm caught error: $_";
+          };
         }
-      }
-    # exit child process
-    exit 0;
-    }
-  else
-    {
-    # we're the main process
-    $gcm_running = AnyEvent->child (pid => $pid, cb => sub
-      {
-      AE::log info => "- - - msg gcm processing finished";
-      undef $gcm_running;
-      }
-    );
-    @gcm_queue = ();
-    }
+      
+      $gcm_running = 0;
+      
+      # send next message if any:
+      &gcm_tim();
+      };
+    } # if (my $rec = pop(@gcm_queue))
   }
 
 1;
